439173da435531ed91f9a18354d93ee8dd7beb39
jalarge <jalarge@gmail.com>
2012-02-17 15:34:28 -0700

    Add StaticPages controller w/ home, about, help & policy.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Qok::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/help"
+
+  get "static_pages/policy"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
