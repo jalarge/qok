3c2bf3bcfae475acfadfb1d0301445293844c8b5
jalarge <jalarge@gmail.com>
2012-02-17 13:27:07 -0700

    Rails new qok -T, git init, notes.txt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qok::Application.config.secret_token = '6ee8d16d3b43a4f5777332ba695a81d6fb9caac802203ea94d36e533ed24c7111571b33302707b9953d5b62aac16d172be532285a33515508bfb4e30172dc86b'
